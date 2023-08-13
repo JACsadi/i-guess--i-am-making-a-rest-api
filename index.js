f7a5d009411c098b6fafba32194cc399910f95a6
2021331096 <2021331096@student.sust.edu>
2023-08-13 23:36:54 +0600

    a

--- index.js
+++ index.js
@@ -3,59 +3,14 @@ const fs = require("fs");
 const app = express();
 const PORT = 8000;
 app.use(express.json());
-const new_data = {
-  name: "Hello",
-};
-app.get("/", async (req, res) => {
-  try {
-    const response = await fetch(
-      "https://raw.githubusercontent.com/Hipo/university-domains-list/master/world_universities_and_domains.json"
-    );
-    if (!response.ok) {
-      throw new Error("Request failed");
-    }
-
-    const jsonData = await response.json();
-    res.status(200).json(jsonData);
-  } catch (error) {
-    console.error("Error fetching data:", error);
-    res.status(500).send("Internal Server Error");
-  }
+let alldata = [];
+app.get("/", (req, res) => {
+  res.status(200).json(alldata);
 });
-app.put("/appendData", async (req, res) => {
-  try {
-    const existingDataResponse = await fetch(
-      "https://example.com/external-data.json"
-    );
-    if (!existingDataResponse.ok) {
-      throw new Error("Request failed");
-    }
-
-    const existingData = await existingDataResponse.json();
-
-    const newData = req.body;
-    existingData.push(newData);
-
-    const updatedResponse = await fetch(
-      "https://example.com/external-data.json",
-      {
-        method: "PUT",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(existingData),
-      }
-    );
-
-    if (!updatedResponse.ok) {
-      throw new Error("Update request failed");
-    }
-
-    res.status(200).json({ message: "Data appended successfully" });
-  } catch (error) {
-    console.error("Error appending data:", error);
-    res.status(500).send("Internal Server Error");
-  }
+app.put("/appendData", (req, res) => {
+  const newData = req.body;
+  alldata.push(newData);
+  res.status(200).json({ message: "Data added successfully", newData });
 });
 // app.post("/addData", (req, res) => {
 //   const newData = req.body;
