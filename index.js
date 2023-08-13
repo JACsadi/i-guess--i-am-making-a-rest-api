7ec605bc1ad442f8f69844cdefd1c41a3760d03a
JACsadi <122394339+JACsadi@users.noreply.github.com>
2023-08-14 00:59:44 +0600

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const fs = require("fs");
 const app = express();
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://your-web-app-domain.com"); // Replace with your actual web app's domain
+  res.header("Access-Control-Allow-Origin", "http://127.0.0.1:5500/index.html"); // Replace with your actual web app's domain
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.header("Access-Control-Allow-Headers", "Content-Type");
   next();
