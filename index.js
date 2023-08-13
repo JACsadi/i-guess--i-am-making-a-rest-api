a898a8e632cb83c90e355e00ce85f3c6236d867c
2021331096 <2021331096@student.sust.edu>
2023-08-13 22:58:48 +0600

    d

--- index.js
+++ index.js
@@ -17,26 +17,26 @@ app.get("/", (req, res) => {
     res.status(200).json(songs);
   });
 });
-app.post("/addData", (req, res) => {
-  const newData = req.body;
-  console.log("hi");
-  fs.readFile("songs.json", "utf8", (err, data) => {
-    if (err) {
-      console.error("Error reading file:", err);
-      return res.status(500).send("Internal Server Error");
-    }
+// app.post("/addData", (req, res) => {
+//   const newData = req.body;
+//   console.log("hi");
+//   fs.readFile("songs.json", "utf8", (err, data) => {
+//     if (err) {
+//       console.error("Error reading file:", err);
+//       return res.status(500).send("Internal Server Error");
+//     }
 
-    const songs = JSON.parse(data);
-    songs.push(newData);
-    fs.writeFile("songs.json", JSON.stringify(songs), "utf-8", (writeErr) => {
-      if (writeErr) {
-        console.error("Error writing file:", writeErr);
-        return res.status(500).send("Internal Server Error");
-      }
-      res.status(201).json({ message: "Data added successfully", newData });
-    });
-  });
-});
+//     const songs = JSON.parse(data);
+//     songs.push(newData);
+//     fs.writeFile("songs.json", JSON.stringify(songs), "utf-8", (writeErr) => {
+//       if (writeErr) {
+//         console.error("Error writing file:", writeErr);
+//         return res.status(500).send("Internal Server Error");
+//       }
+//       res.status(201).json({ message: "Data added successfully", newData });
+//     });
+//   });
+// });
 app.listen(PORT, () => {
   console.log(`API is running at http://localhost:${PORT}`);
 });
